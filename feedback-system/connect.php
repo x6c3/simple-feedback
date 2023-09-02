8eeb216a18521f2e413025ac6d9f9d2cc9238334
jxy <90693790+jxytherealone@users.noreply.github.com>
2023-09-02 11:57:59 +0530

    Update connect.php

--- feedback-system/connect.php
+++ feedback-system/connect.php
@@ -1,14 +1,3 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "feedback_system";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = new mysqli("localhost","root", "", "feedback_system");
 ?>
